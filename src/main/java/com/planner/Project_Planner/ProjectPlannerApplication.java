b00752a17b3dee16179267636a4c48919edb8a31
Stefanos Arapoglou <arapsteflol@gmail.com>
2025-09-11 19:24:40 +0300

    Initial commit

---
+++ src/main/java/com/planner/Project_Planner/ProjectPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.planner.Project_Planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planner/Project_Planner/ProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planner.Project_Planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
